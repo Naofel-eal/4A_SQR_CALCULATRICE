5133fafc41408d1be833b4cd2a0214effb2c5360
AnisseO <93181410+AnisseO@users.noreply.github.com>
2023-01-13 11:41:31 +0100

    Ajout transaction
    Ajout de transaction nous permettant de tester E3

--- TP.py
+++ TP.py
@@ -12,12 +12,15 @@ def home():
 
 transaction = [
     (0, 1, '14/02/2023', 380),
-    (1, 0, '28/01/2023', 52)
+    (1, 0, '28/01/2023', 52),
+    (1, 2, '11/08/2022', 12),
+    (0, 1, '16/12/2022', 33)
 ]
 
 people = [
 	Person(id=0, firstname="Naofel", name="EL ALOUANI", solde=300),
- 	Person(id=1, firstname="Anisse", name="OUTSSAKKI", solde=100)
+ 	Person(id=1, firstname="Anisse", name="OUTSSAKKI", solde=100),
+    Person(id=2, firstname="Omar", name="AMANA", solde=1200)
 ]
 
 @app.route('/tuple', methods=['GET'])
@@ -25,13 +28,9 @@ def get_tuple():
 	tran = sort_dates()
 	response = "<h1>Transactions: </h1><ul>"
 	for transac in tran:
-		response += "<li>" + transac[2] + " : " + transac[0] + " a viré " + str(transac[3]) + "€ à " + transac[1] +"</li>"
-	response += "</ul>"
+				response += "<li>" + transac[2] + " : " + getPersonByID(transac[0]).firstname + " a viré " + str(transac[3]) + "€ à " + getPersonByID(transac[1]).firstname +"</li>"
 	return response
 
-def sort_dates():
-	transac = sorted(transaction, key=lambda x: datetime.strptime(x[2], '%d/%m/%Y'))
-	return transac
 
 @app.route("/add", methods=['POST'])
 def add():
@@ -53,6 +52,17 @@ def getPersonByID(id: int) -> Person:
         if person.id == id:
             return person
 
+def sort_dates():
+	transac = sorted(transaction, key=lambda x: datetime.strptime(x[2], '%d/%m/%Y'))
+	return transac
 	
 if __name__ == '__main__':
     app.run()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+            print("Passed argument not supported ! Supported argument : check_syntax")
+            exit(1)
+    app.run(debug=True)
