59ae096391ec685e94b8b15a1204f5381f10254b
Naofel EL ALOUANI <naofel.eal@gmail.com>
2023-01-13 18:00:40 +0100

    route to add csv

--- TP.py
+++ TP.py
@@ -1,30 +1,15 @@
 #git stash; git pull; git stash pop;
 
-from flask import Flask, jsonify, request
-from datetime import datetime
-import json
-from types import SimpleNamespace
+from flask import Flask, request
 from classes.Person import Person
 from utils.functions import *
-
-app = Flask(__name__)
+import sys
+import csv
+import os
 
 @app.route('/')
 def home():
 	return '''<h1>Salut !</h1>'''
-
-transaction = [
-    (0, 1, '14/02/2023', 380),
-    (1, 0, '28/01/2023', 52),
-    (1, 2, '11/08/2022', 12),
-    (0, 1, '16/12/2022', 33)
-]
-
-people = [
-	Person(id=0, firstname="Naofel", name="EL ALOUANI", solde=300),
- 	Person(id=1, firstname="Anisse", name="OUTSSAKKI", solde=100),
-    Person(id=2, firstname="Omar", name="AMANA", solde=1200)
-]
 
 @app.route("/add", methods=['POST'])
 def add():
@@ -42,13 +27,27 @@ def add():
     addTransaction(P1=P1, P2=P2, date=date, sum=value)
     return data
 
-@app.route('/tuple', methods=['GET'])
+@app.route('/list', methods=['GET'])
 def get_tuple():
-	tran = sort_dates()
-	response = "<h1>Transactions: </h1><ul>"
-	for transac in tran:
-				response += "<li>" + transac[2] + " : " + getPersonByID(transac[0]).firstname + " a viré " + str(transac[3]) + "€ à " + getPersonByID(transac[1]).firstname +"</li>"
-	return response
+	transactions = sort_dates()
+	return stringifyTransactions()
+
+@app.route('/csv', methods=['POST'])
+def uploadCSV():
+    data = []
+    if request.method == 'POST':
+        if request.files:
+            file = request.files['transactions']
+            filepath = os.path.join(app.config['FILE_UPLOADS'], file.filename)
+            file.save(filepath)
+
+            with open(filepath) as csv_file:
+                reader = csv.reader(csv_file)
+                for row in reader:
+                    addTransaction(P1=getPersonByID(int(row[0])), P2=getPersonByID(int(row[1])), sum=row[3], date=row[2])
+        else:
+            print("No")
+    return stringifyTransactions()
 
 	
 if __name__ == '__main__':
--- utils/functions.py
+++ utils/functions.py
@@ -1,3 +1,30 @@
+from classes.Person import Person
+from datetime import datetime
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+app.config['FILE_UPLOADS'] = os.path.dirname(os.path.abspath(__file__))
+
+transactions = [
+    (0, 1, '14/02/2023', 380),
+    (1, 0, '28/01/2023', 52),
+    (1, 2, '11/08/2022', 12),
+    (0, 1, '16/12/2022', 33)
+]
+
+people = [
+	Person(id=0, firstname="Naofel", name="EL ALOUANI", solde=300),
+ 	Person(id=1, firstname="Anisse", name="OUTSSAKKI", solde=100),
+    Person(id=2, firstname="Omar", name="AMANA", solde=1200)
+]
+
+def stringifyTransactions() -> str:
+    response = "<h1>Transactions: </h1><ul>"
+    for transac in transactions:
+        response += "<li>" + transac[2] + " : " + getPersonByID(transac[0]).firstname + " a viré " + str(transac[3]) + "€ à " + getPersonByID(transac[1]).firstname +"</li>"
+    return response
+
 def getPersonByNames(firstname, name) -> Person:
     for person in people:
         if person.name == name and person.firstname == firstname:
@@ -11,12 +38,13 @@ def getPersonByID(id: int) -> Person:
     for person in people:
         if person.id == id:
             return person
+    print("Person not found !")
+    return None
 
 def addTransaction(P1 : Person, P2 : Person, sum : float, date : str):
     tuple = (P1.id, P2.id, date, sum)
-    print(tuple)
-    transaction.append(tuple)
+    transactions.append(tuple)
 
 def sort_dates():
-	transac = sorted(transaction, key=lambda x: datetime.strptime(x[2], '%d/%m/%Y'))
+	transac = sorted(transactions, key=lambda x: datetime.strptime(x[2], '%d/%m/%Y'))
 	return transac
